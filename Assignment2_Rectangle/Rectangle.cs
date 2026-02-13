1cec1467571d67e493692468ea4a4babe2d045f4
CB2604 <christinebonneville@outlook.com>
2026-02-12 23:29:38 -0500

    Added empty project files and solution structure

---
+++ Assignment2_Rectangle/Rectangle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Assignment2_Rectangle
+{
+    public class Rectangle
+    {
+        
+
+    }
+}
---
+++ Assignment2_Rectangle_Tests/RectangleTests.cs
@@ -0,0 +1,11 @@
+﻿using Assignment2_Rectangle; // Make sure your test project references the main project
+using NUnit.Framework;
+
+namespace Assignment2_Rectangle_Tests
+{
+    [TestFixture]
+    public class RectangleTests
+    {
+        
+    }
+}
