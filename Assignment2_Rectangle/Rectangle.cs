6c459943ad5b180f4e0cbf99099f12e389adaf85
CB2604 <christinebonneville@outlook.com>
2026-02-12 23:44:38 -0500

    Implement Rectangle.cs properties and methods.

--- Assignment2_Rectangle/Rectangle.cs
+++ Assignment2_Rectangle/Rectangle.cs
@@ -4,7 +4,61 @@ namespace Assignment2_Rectangle
 {
     public class Rectangle
     {
-        
+        // 2 private int
+        private int length;
+        private int width;
+
+        //Default constructor
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        //Non-default constructor for user values
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        // Get Length method
+        public int GetLength()
+        {
+            return length;
+        }
+
+        // Set Length method
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+
+        // Get Width method
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        // Set Width method
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+
+        // Get Perimeter method
+        public int GetPerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        // Get Area method
+        public int GetArea()
+        {
+            return length * width;
+        }
 
     }
 }
